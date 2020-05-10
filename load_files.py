474b847a840c8a5518b6da4f823c34f5ea907da9
Unknown <francoo_0011@hotmail.es>
2020-05-10 19:41:24 -0400

    convertir sub a texto plano

--- load_files.py
+++ load_files.py
@@ -1,5 +1,18 @@
 import pysubs2
+import pysrt
 archivo = 'subtitle.ass'
 subs = pysubs2.load(archivo, encoding='utf-8')
+
 for line in subs:
     print(line.text)
+    textoplano = line.text
+    texto = open('textoplano.txt', 'a')
+    texto.write(textoplano)
+    texto.close()
+
+
+
+
+
+
+
